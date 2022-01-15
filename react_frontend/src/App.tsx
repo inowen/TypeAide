674d27b23533f65d3e3ebbf362534ad0c47309b1
inowen <inoah@correo.ugr.es>
2022-01-15 20:20:13 +0100

    React: Use classes extending Component instead of functions.

--- react_frontend/src/App.tsx
+++ react_frontend/src/App.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import './App.css';
 
-function App() {
 
-  return (
-    <div className="App">
-      <input type="text" onKeyDown={(event) => {
-        console.log("Key pressed " + event.key)
-        if (event.ctrlKey) {
-          console.log("Control key is currently active");
-        }
-       }
-      }/>
+class App extends React.Component {
 
-    </div>
-  );
+  constructor(props : any) {
+    super(props);
+  }
+
+  render() {
+    return <div className="App">
+        This is some text.
+      </div>
+  }
 
 }
 
