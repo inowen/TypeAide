e6136c9e852f4f5e0701c27bbe7c8a334cb869d0
inowen <inoah@correo.ugr.es>
2022-01-15 20:20:47 +0100

    Feat: Typing window component

---
+++ react_frontend/src/TextWindow.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class TextWindow extends React.Component {
+    constructor(props: any) {
+        super(props);
+    }
+
+    render() {
+        return <div className="TextWindow"/>
+    }
+}
+
+export default TextWindow;
