5eb9008a61dd9a457cc492337c41c30cf5dadadf
inowen <inoah@correo.ugr.es>
2022-01-16 21:56:09 +0100

    Test: Getting to know bootstrap

--- react_frontend/src/App.tsx
+++ react_frontend/src/App.tsx
@@ -4,13 +4,20 @@ import './App.css';
 
 class App extends React.Component {
 
-  constructor(props : any) {
+  constructor(props: any) {
     super(props);
   }
 
   render() {
-    return <div className="App">
+    return <div className="alert alert-success">
         This is some text.
+        <div className="alert alert-warning">This is a subdiv with a warning</div>
+        <div className="container-fluid">
+          <div className="strong">This is strong text</div>
+          <div className="lead">This is lead text</div>
+          <button className="btn large btn-primary">Primary button</button>
+          <button className="btn-default">Secondary small button</button>
+        </div>
       </div>
   }
 
