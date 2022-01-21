455aaf8f82983add11b0760f506b5d2eff6eef54
inowen <inoah@correo.ugr.es>
2022-01-21 11:03:07 +0100

    Remove old navbar

--- react_frontend/src/App.tsx
+++ react_frontend/src/App.tsx
@@ -1,42 +1,23 @@
 import React, {useState} from 'react';
-import {NavbarToggler, Collapse, NavItem} from 'reactstrap';
 import 'reactstrap';
-import { Button, Container, Nav, Navbar, NavbarBrand, NavbarText, NavLink, Row, Col} from 'reactstrap';
+import { Container } from 'reactstrap';
 import './App.css';
+import NavigationBar from './NavigationBar';
 
-function Header() {
-  return <div className="">
-    <Navbar color='dark' dark expand={false} role="navigation">
-      <Nav className="mr-auto" navbar>
-        <Container fluid="md">
-          <Row>
-          <Col>
-          <NavItem>
-            <NavLink href="/">Typing</NavLink>
-          </NavItem>
-          </Col>
-          <Col>
-          <NavItem>
-            <NavLink href="/">Stats</NavLink>
-          </NavItem>
-          </Col>
-          <Col>
-          <NavItem>
-            <NavLink href="/">About</NavLink>
-          </NavItem>
-          </Col>
-          <Col>
-          <NavItem>
-            <NavLink href="/">Login</NavLink>
-          </NavItem>
-          </Col>
-          </Row>
-        </Container>
-      </Nav>
-    </Navbar>
-  </div>
+
+function App() {
+  let type: string, setType: any;
+  [type, setType] = useState("btn-success");
+  return <div className="App">
+    <NavigationBar/>
+    <Container className="mt-2">
+      <h2 className="text-info">This is a header</h2>
+      <ChangingButton type={type} setType={setType}/>
+    </Container>
+  </div>;
 }
 
+// This is testing code
 function ChangingButton(props: any) {
       return <button className={props.type} onClick={
         () => props.setType((prev:string) => {
@@ -45,16 +26,5 @@ function ChangingButton(props: any) {
       }>{props.type} button!</button>
 }
 
-function App() {
-  let type: string, setType: any;
-  [type, setType] = useState("btn-success");
-  return <div className="App">
-    <Header/>
-    <Container className="mt-2">
-      <h2 className="text-info">This is a header</h2>
-      <ChangingButton type={type} setType={setType}/>
-    </Container>
-  </div>;
-}
 
 export default App;
