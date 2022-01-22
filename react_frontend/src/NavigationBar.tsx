774ed823dd299c99bb01ef1d55573641f383229d
inowen <inoah@correo.ugr.es>
2022-01-22 21:51:11 +0100

    Remove my navbar implementation again
    Reactstrap's thing honestly looks a lot better, if I can just get the components to go where they are supposed to

--- react_frontend/src/NavigationBar.tsx
+++ react_frontend/src/NavigationBar.tsx
@@ -1,17 +1,10 @@
+import { Navbar } from 'reactstrap';
 import './navbar.css'
 
 function NavigationBar() {
-  return <div className="nav nav-bar navigationbar">
-      <div className="navbar-container">
-        <div className="navbar-left">
-          <button className="btn logo-btn with-margin">Logo</button>
-          <button className="btn with-margin highlight-border navbar-button">Other</button>
-        </div>
-        <div className="navbar-right">
-          <button className="btn login-btn with-margin navbar-button">Login</button>
-        </div>
-      </div>
-  </div>
+  return <Navbar>
+
+  </Navbar>
 }
 
 
