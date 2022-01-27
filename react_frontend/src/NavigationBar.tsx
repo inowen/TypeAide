ac6fd5cc0417f6adcb10b63e8d9f9175a12992ad
inowen <inoah@correo.ugr.es>
2022-01-27 17:13:46 +0100

    Feat: Added logo to navbar, adjusted spacing

--- react_frontend/src/NavigationBar.tsx
+++ react_frontend/src/NavigationBar.tsx
@@ -1,13 +1,14 @@
 import  { Button, Nav, Navbar, NavbarBrand, NavLink } from 'react-bootstrap';
+import logo from "./resources/logo_typinglite.png";
 
 function NavigationBar() {
   return <Navbar variant="dark" bg="dark">
     <NavbarBrand style={{marginLeft:"1%"}}>
-      Brand
+      <img src={logo} style={{height:"50px", width:"150px"}}/>
     </NavbarBrand>
-    <Nav>
+    <Nav style={{marginLeft:"50px"}}>
       <NavLink href="#">Typing </NavLink>
-      <NavLink href="#">About</NavLink>
+      <NavLink href="#" style={{marginLeft:"20px"}}>About</NavLink>
     </Nav>
     <div style={{position:"absolute", right:"1%"}}>
     <Nav>
