1dfc2fafcef57eaae4d65cdd17ab9eac0a9a8a0d
inowen <inoah@correo.ugr.es>
2022-02-02 17:32:42 +0100

    Backup script for mongodb

--- mongodb/make_backup.sh
+++ mongodb/make_backup.sh
@@ -1,2 +1,13 @@
-# Just mongoexports the typinglite database into the backups folder.
+# Create a folder with the current date in ./backups, mongorestore --host mongodb to that folder 
+# Step by step.
 
+# Create the date string
+date_str=$(date +%Y_%m_%d_%H_%M_%S)
+
+# Create the folder
+foldername=$(echo $date_str)_backup
+echo "Saving to folder named $foldername (format is Y_M_D_Hour_Min_Second)."
+mkdir ./backups/$foldername
+
+# Restore to that folder
+mongorestore --host mongodb ./backups/$foldername
