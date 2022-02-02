929dbfb17e6658e14fa21c9712a28ac226d57959
inowen <inoah@correo.ugr.es>
2022-02-02 14:02:40 +0100

    Feat: Initialization script for mongo, ran from mongoinit container

--- mongodb/scripts/init_db.sh
+++ mongodb/scripts/init_db.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
-echo "test, this is init_db"
-#if mongo databases grep typinglite == 0:
-#	mongoImport from default;
+# Wait for db to initialize
+output=$(mongo --host mongodb --eval "db.adminCommand({listDatabases:1})")
+grp=$(echo $output | grep "Error");
+while [[ ! -z $grp ]];
+do
+    sleep 1
+    echo "Re-attempting connection"
+    output=$(mongo --host mongodb --eval "db.adminCommand({listDatabases:1})")
+    grp=$(echo $output | grep "Error");
+done
+echo "Connected!"
+
+# If the db is empty, initialize it
+if [[ $(echo $output | grep '"name" : "typinglite"') == "" ]]; then
+    echo "The db is empty, checking for default content folder";
+    # Check if there is something in the default folder, otherwise exit
+    if [[ $(ls /app/default_content | wc -l) == "0" ]]; then
+        echo "The default content folder is empty, aborting initialization script"
+        exit 1;
+    fi;
+    echo "Default content folder exists and isn't empty."
+    echo "Using mongorestore on default content folder."
+    mongorestore --host mongodb /app/default_content
+else 
+    echo "Typinglite database already present, not initializing."
+fi;
