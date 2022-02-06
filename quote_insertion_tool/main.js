ae934cec712375d6eca04e3b3f815e4cb2426bd6
inowen <inoah@correo.ugr.es>
2022-02-06 15:13:05 +0100

    Feat: Useful feedback about what the inserter tool did

--- quote_insertion_tool/main.js
+++ quote_insertion_tool/main.js
@@ -16,16 +16,16 @@ function process_file(filename) {
 
     // Process each quote in the file individually
     json_array.forEach((quote) => {
-        console.log("\tChecking quote: " + quote); 
+        console.log("\tQuote: " + quote.substring(0,50) + "..."); 
 
         // Run tests
         let tester = new Tester();
         let failed = tester.whichTestFails(quote);
         if (failed != null) {
-            console.log("\t\tFailed test: " + failed);
+            console.log("\tFailed test: " + failed);
         }
         else {
-            console.log("\t\tTests succesful, adding to upload list.");
+            console.log("\tPasses all tests.");
             let hash = crypto.createHash('md5').update(quote).digest('hex');
             res_quotes.push({text:quote, length:(quote.length), _id:hash});
         }
@@ -64,25 +64,42 @@ async function main() {
         console.error("You must pass in one or more file names.");
         exit(-1);
     }
+    // Counters for report 
+    let num_total_quotes = 0;
+    let num_total_passed = 0;
+    let num_total_inserted = 0;
+    let num_total_duplicates = 0;
+
     let files = argv;
     files.shift(); files.shift();
+    console.log("-------- PROCESSING YOUR FILES --------");
     for (let i=0; i<files.length; ++i) {
         let filename = files[i];
         try {
-            console.log("Processing " + filename);
+            console.log("*** FILE: " + filename);
             let quote_objects = process_file(filename);
             // Write to the database
             console.log("Attempting to write " + quote_objects.length + " quotes to the database.");
             let num_inserted = await write_to_db(quote_objects);
             console.log("Inserted " + num_inserted + " texts (the rest were duplicates).");
+            // Accounting
+            num_total_passed += quote_objects.length;
+            num_total_inserted += num_inserted;
+            num_total_duplicates += quote_objects.length - num_inserted;
+            // Read in file (again) to count how many quotes there were to begin with
+            // (quickest way to do this, not the most efficient)
+            num_total_quotes += JSON.parse(fs.readFileSync(filename)).length;
         }
         catch (exception) {
             console.error("Couldn't read file: " + filename);
-            // debug
-            console.log(exception);
         }
         console.log("");
     }
+
+    console.log(" -------- SUMMARY -------- ");
+    console.log("Failed tests: " + (num_total_quotes-num_total_passed) + "/" + num_total_quotes)
+    console.log("Quotes inserted: " + num_total_inserted + "/" + num_total_quotes);
+    console.log("Duplicates: " + num_total_duplicates + "/" + num_total_quotes);
 }
 
 await main();
