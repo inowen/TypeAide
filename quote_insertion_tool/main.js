bef856e20b9222f92319716ddefbe4318e8c3d24
inowen <inoah@correo.ugr.es>
2022-02-06 14:49:09 +0100

    Feat: Write passing quotes to db

--- quote_insertion_tool/main.js
+++ quote_insertion_tool/main.js
@@ -35,21 +35,25 @@ function process_file(filename) {
 
 /**
  * Write an array of quote objects to the database
+ * @returns Number of quotes that were inserted
  */
 async function write_to_db(quote_objects) {
     let url = "mongodb://localhost:27017/";
     let MongoClient = mongodb.MongoClient;
     let connection = await MongoClient.connect(url);
     let db = connection.db("typinglite");
+    let insert_counter = 0;
 
     for (let i_quote=0; i_quote<quote_objects.length; ++i_quote) {
         let quote = quote_objects[i_quote];
-        // Check if there's one in the db already
-
-        // Insert into db
+        let found = await db.collection("quotes").findOne({_id:quote._id});
+        if (found == null) {
+            await db.collection("quotes").insertOne(quote);
+            insert_counter += 1;
+        }
     }
-
     connection.close();
+    return insert_counter;
 }
 
 
@@ -67,10 +71,10 @@ async function main() {
         try {
             console.log("Processing " + filename);
             let quote_objects = process_file(filename);
-            console.log(quote_objects);
             // Write to the database
-            console.log("Writing " + quote_objects.length + " quotes to the database.");
-            await write_to_db(quote_objects);
+            console.log("Attempting to write " + quote_objects.length + " quotes to the database.");
+            let num_inserted = await write_to_db(quote_objects);
+            console.log("Inserted " + num_inserted + " texts (the rest were duplicates).");
         }
         catch (exception) {
             console.error("Couldn't read file: " + filename);
