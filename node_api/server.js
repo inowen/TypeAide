a3412c9410036b23926b1309188543f5ffea0d29
inowen <inoah@correo.ugr.es>
2022-02-07 13:09:53 +0100

    Feat: randomquote api now returns a quote from the db

--- node_api/server.js
+++ node_api/server.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var app = express();
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
 
-app.get('/api/v1/a', function(req, res) {
-    res.end("This is the a API");
+// Add responses to requests 
+app.get('/api/v1/randomquote', function(req, res) {
+    let url = "mongodb://localhost:27017/";
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        let dbo = db.db("typinglite");
+        dbo.collection("quotes").findOne({}, (err, result) => {
+            if (err) throw err;
+            console.log(result.text);
+            res.end(result.text);
+            db.close();
+        });
+    });
 });
 
-app.get('api/v1/b', function(req, res) {
-    res.end("This is the b API");
-})
+app.get('/api/v1/test', function(req, res) {
+    res.end("Seems to work fine.");
+});
+
+// Create the server to listen to them 
+let server = app.listen(8080);
