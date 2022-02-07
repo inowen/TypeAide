779eb4e824b1e4424f5b88156f6af4e873262654
inowen <inoah@correo.ugr.es>
2022-02-07 13:26:17 +0100

    Feat: Make the randomquote api quote selection random
    It's not a good solution: For n quotes, worst case it skips past all n quotes, reading their indices, instead of doing direct access. But for now it's going to have to be good enough

--- node_api/server.js
+++ node_api/server.js
@@ -6,13 +6,17 @@ var MongoClient = mongodb.MongoClient;
 // Add responses to requests 
 app.get('/api/v1/randomquote', function(req, res) {
     let url = "mongodb://localhost:27017/";
-    MongoClient.connect(url, function(err, db) {
+    MongoClient.connect(url, async function(err, db) {
         if (err) throw err;
         let dbo = db.db("typinglite");
-        dbo.collection("quotes").findOne({}, (err, result) => {
+
+        let num_quotes = await dbo.collection("quotes").countDocuments();
+        let rnd = Math.floor(Math.random() * num_quotes);
+        console.log("Count: " + num_quotes + ", rnd: " + rnd);
+        dbo.collection("quotes").find().limit(1).skip(rnd).toArray((err, result) => {
             if (err) throw err;
-            console.log(result.text);
-            res.end(result.text);
+            console.log(result);
+            res.end(result[0].text);
             db.close();
         });
     });
