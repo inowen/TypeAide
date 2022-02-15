9935624dcfdec88907b00d0a4b83b96276728357
inowen <inoah@correo.ugr.es>
2022-02-15 17:05:39 +0100

    Make footer smaller, and its font visible

--- react_frontend/src/Footer.tsx
+++ react_frontend/src/Footer.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
 
-const footerStyle = {
-    background: "black",
-    fontWeight: "bold"
-};
-
 function Footer() {
     return (
-        <div style={footerStyle} className="footer">
+        <div className="footer bg-dark">
             Footer
         </div>
     );
