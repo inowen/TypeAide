8e27b60dfad453af772d982e60fb0a7d069b60d6
inowen <inoah@correo.ugr.es>
2022-02-15 16:46:36 +0100

    Frontend: Add footer to site

--- react_frontend/src/App.tsx
+++ react_frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import NavigationBar from './NavigationBar';
+import Footer from './Footer';
+import TypingArea from './TypingArea';
 
 
 function App() {
   return <div className="App">
     <NavigationBar/>
+    <Footer/>
   </div>;
 }
 
--- react_frontend/src/Footer.tsx
+++ react_frontend/src/Footer.tsx
@@ -12,3 +12,5 @@ function Footer() {
         </div>
     );
 }
+
+export default Footer;
