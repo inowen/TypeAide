4e7f722676d1061e8064b5b2764e90e9b8dd6c34
inowen <inoah@correo.ugr.es>
2022-02-16 09:19:14 +0100

    Use fetch API to request data from server

--- react_frontend/src/App.tsx
+++ react_frontend/src/App.tsx
@@ -9,6 +9,17 @@ function App() {
   return <div className="App">
     <NavigationBar/>
     <TypingArea/>
+    <div className="just-testing">
+      <button className="testButton" onClick={() => {
+        // Spin up the docker environment for this
+        const myAsyncFunc = async () => {
+          let url = "/api/v1/randomquote";
+          let response = await fetch(url);
+          console.log(response);
+        }
+        myAsyncFunc();
+      }}>Testing fetch</button>
+    </div>
     <Footer/>
   </div>;
 }
