8e10fa02156aa67da905349f323ed3b369ba7d10
inowen <inoah@correo.ugr.es>
2022-02-18 09:55:10 +0100

    Feat: Automatically focus typing area on loading page

--- react_frontend/src/TypingArea.tsx
+++ react_frontend/src/TypingArea.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useRef, useEffect } from 'react';
 import TextWindow from './TextWindow';
 import './TypingArea.css';
 
 function TypingArea() {
+    const thisRef = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+        if (thisRef.current != null) {
+            thisRef.current.focus();
+        }
+    }, []);
     return (
-        <div className="typingarea">
+        <div className="typingarea" ref={thisRef} tabIndex={1}>
             <div className="vcenterflex">
                 <TextWindow/>
             </div>
